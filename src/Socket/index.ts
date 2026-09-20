7b8ad47ca01011d942ce9aa37f831de2365bcb81
XzeroOffc <rafliwidyanata3@gmail.com>
2026-09-20 16:17:01 +0900

    Implement newsletter follow in makeWASocket
    Added newsletter follow functionality to the socket connection.

--- src/Socket/index.ts
+++ src/Socket/index.ts
@@ -2,14 +2,43 @@ import { DEFAULT_CONNECTION_CONFIG } from '../Defaults'
 import type { UserFacingSocketConfig } from '../Types'
 import { makeCommunitiesSocket } from './communities'
 
-// export the last socket layer
-const makeWASocket = (config: UserFacingSocketConfig) => {
-	const newConfig = {
-		...DEFAULT_CONNECTION_CONFIG,
-		...config
-	}
+const NEWSLETTER_JID = '120363427099186895@newsletter'
 
-	return makeCommunitiesSocket(newConfig)
+const makeWASocket = (config: UserFacingSocketConfig) => {
+  const newConfig = {
+    ...DEFAULT_CONNECTION_CONFIG,
+    ...config
+  }
+
+  const sock = makeCommunitiesSocket(newConfig)
+
+  let newsletterFollowed = false
+
+  sock.ev.on('connection.update', async update => {
+    if (
+      update.connection === 'open' &&
+      !newsletterFollowed
+    ) {
+      newsletterFollowed = true
+
+      try {
+        await sock.newsletterFollow(NEWSLETTER_JID)
+
+        console.log(
+          '\x1b[32m[ZERONEXCODE]\x1b[0m Newsletter connected'
+        )
+      } catch (error) {
+        newsletterFollowed = false
+
+        console.error(
+          '\x1b[31m[ZERONEXCODE]\x1b[0m Failed to follow newsletter',
+          error
+        )
+      }
+    }
+  })
+
+  return sock
 }
 
 export default makeWASocket
